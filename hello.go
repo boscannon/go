f22c59a2ff4460c519c7136e9c40c0261597096a
cannon <cannon>
2023-02-21 22:45:54 +0800

    a

--- hello.go
+++ hello.go
@@ -1,7 +1,24 @@
-package main 
+package main
 
-import "fmt" 
+import "fmt"
 
-func main() { 
-    fmt.Println("Hello, World!") 
+func main() {
+	// var (
+  //   a int = 15
+  //   b bool = false
+  //   c string = "6xxx5"
+  //   d [5]int
+	// )
+
+	m := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	
+	fmt.Println(m)
+
+	for k, v := range m {
+		fmt.Printf("key=%s, value=%d\n", k, v)
+	}
 }
