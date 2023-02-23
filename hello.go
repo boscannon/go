0fa1fb4d72440c9d7c4b22fd0e50bc44c43bc56f
cannon <cannon>
2023-02-23 16:07:58 +0800

    a

--- hello.go
+++ hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func add(x int, y int) int {
+	fmt.Printf("x=%d,y=%d\n", x, y)
+	return x + y;
+}
+
 func main() {
 	// var (
   //   a int = 15
@@ -10,15 +15,41 @@ func main() {
   //   d [5]int
 	// )
 
-	m := map[string]int{
-		"a": 1,
-		"b": 2,
-		"c": 3,
+	m := map[string]map[string]int{
+		"a": map[string]int{
+			"a1": 1,
+			"a2": 2,
+		},
+		"b": map[string]int{
+			"b1": 1,
+			"b2": 2,
+		},
+		"c": map[string]int{
+			"c1": 1,
+			"c2": 2,
+		},
+		"d": map[string]int{
+			"d1": 1,
+			"d2": 2,
+		},
 	}
-	
-	fmt.Println(m)
+
+	// fmt.Println(m["a"])
+
+	// fmt.Println(add(11, 22));
+
+	// if name, ok := m["a"]; ok {
+	// 	fmt.Println(name, ok)
+	// }
+
 
 	for k, v := range m {
-		fmt.Printf("key=%s, value=%d\n", k, v)
+		fmt.Printf("key=%s\n", k)
+		sum := 0;
+		for k1, item := range v {			
+			sum = add(sum, item)
+			fmt.Printf("sub\nkey=%s,value=%d\n", k1, item)
+		}
+		fmt.Printf("total=%d\n", sum)
 	}
 }
